e02e3a20bc486ae3fdf01e7dc15b3a5440bd81d6
Asim Ahmed Khan <aasimahmedkhan05@gmail.com>
2020-07-04 17:48:47 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Child from './child';
+import {TransactionProvider} from './transContext';
+
+function App() {
+  return (
+    <TransactionProvider>
+        <Child/>
+    </TransactionProvider>
+   
+  );
+      
+
+}
+
+export default App;
---
+++ src/transContext.js
@@ -0,0 +1,45 @@
+import React, { createContext, useReducer } from "react";
+import TransactionReducer from './transReducer';
+
+const initialTransactions = [
+    { amount: 500, desc: "Cash" },
+    { amount: -60, desc: "Lunch" },
+    { amount: 100, desc: "Deposit" },
+    { amount: -150, desc: "Utility Bill"},
+
+  ]
+
+export const TransactionContext = createContext(initialTransactions);
+
+
+
+export const TransactionProvider = ({children})=> {
+    let [state, dispatch] = useReducer(TransactionReducer, initialTransactions);
+
+     function addTransaction(transObj){
+         dispatch({
+           type: "ADD_TRANSACTION",
+           payload: {
+               amount: transObj.amount,
+               desc: transObj.desc
+           },
+         })
+
+         
+     }
+    
+     return(
+         <TransactionContext.Provider value={{
+            transactions: state,
+            addTransaction
+            
+
+         }}>
+             {children}
+
+         </TransactionContext.Provider>
+     )
+
+}
+       
+  
